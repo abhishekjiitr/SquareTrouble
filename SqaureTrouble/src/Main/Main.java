0105b638985826f36380fc7dce6de13d8d3350fa
jaisingh <abhi2254015@gmail.com>
2015-09-12 00:10:35 +0530

    Gadbad
    Mistake

--- SqaureTrouble/src/Main/Main.java
+++ SqaureTrouble/src/Main/Main.java
@@ -7,10 +7,10 @@ public class Main
 
 	public static void main(String[] args) throws InterruptedException, IOException
 	{
-		//Splash sp = new Splash();
-		//Thread.sleep(3000);
+		Splash sp = new Splash();
+		Thread.sleep(3000);
 		StartScreen startScreen = new StartScreen();
-		//sp.dispose();	
+		sp.dispose();	
 		
 		//PlayerInfo pInfo = new PlayerInfo();
 		//Game gm = new Game();
